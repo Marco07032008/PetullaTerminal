399852b1df83e0aadd16a0f6c80a5868654bcd2f
Marco07032008 <108406384+Marco07032008@users.noreply.github.com>
2023-08-31 18:27:47 +0200

    Add files via upload

---
+++ Petulla.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+
+def install_figlet():
+    # Check if figlet is installed
+    result = subprocess.run(["which", "figlet"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print("Installing figlet...")
+        subprocess.run(["sudo", "apt", "install", "figlet", "-y"])
+
+def main():
+    # Check if the script is run with root privileges
+    if os.geteuid() != 0:
+        print("You must run this as root!")
+        sys.exit(1)
+
+    # Install figlet if not already installed
+    install_figlet()
+
+    # Display a message using figlet
+    subprocess.run(["figlet", "Petulla Terminal"])
+
+if __name__ == "__main__":
+    main()
